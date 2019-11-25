05690d76c43edd31384b715db1ed393b07ab2266
Justin Liu <justin.liu@myob.com>
2019-11-25 16:09:06 +1100

    Add comment route

--- src/server.js
+++ src/server.js
@@ -32,4 +32,11 @@ app.post('/api/articles/:name/upvote', (req, res) => {
     .send(`${articleName} has ${articlesInfo[articleName].upvotes} upvotes`);
 });
 
+app.post('/api/articles/:name/add-comment', (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+  articlesInfo[articleName].comments.push({ username, text });
+  res.status(200).send(articlesInfo[articleName]);
+});
+
 app.listen(8000, () => console.log('Listening on port 8000'));
