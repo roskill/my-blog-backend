7f75aac449260799555c82bfd8ad803a3f0baee4
Justin Liu <justin.liu@myob.com>
2019-11-26 18:05:18 +1100

    Complete refactor for Mongo DB

--- src/server.js
+++ src/server.js
@@ -57,11 +57,24 @@ app.post('/api/articles/:name/upvote', async (req, res) => {
   }, res);
 });
 
-app.post('/api/articles/:name/add-comment', (req, res) => {
-  const { username, text } = req.body;
-  const articleName = req.params.name;
-  articlesInfo[articleName].comments.push({ username, text });
-  res.status(200).send(articlesInfo[articleName]);
+app.post('/api/articles/:name/add-comment', async (req, res) => {
+  withDB(async db => {
+    const { username, text } = req.body;
+    const articleName = req.params.name;
+    const articleInfo = await db
+      .collection('articles')
+      .findOne({ name: articleName });
+    await db.collection('articles').updateOne(
+      { name: articleName },
+      {
+        $set: { comments: articleInfo.comments.concat({ username, text }) }
+      }
+    );
+    const updatedArticleInfo = await db
+      .collection('articles')
+      .findOne({ name: articleName });
+    res.status(200).json(updatedArticleInfo);
+  }, res);
 });
 
 app.listen(8000, () => console.log('Listening on port 8000'));
